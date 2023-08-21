5da889b6043e5d03130a16c853d1893cb56f68fd
Ole007Chi <hatmirzj@hotmail.com>
2023-08-21 05:11:43 -0700

    101-main.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H_INCLUDE
+#define MAIN_H_INCLUDE
+
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void rev_string(char *s);
+void puts2(char *str);
+int _putchar(char);
+void print_rev(char *s);
+void print_alphabet(void);
+void _puts(char *str);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int _strlen(char *s);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int);
+void print_times_table(int);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
+void print_number(int n);
+
+#endif
